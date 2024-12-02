e56d259f445ff3b9af53463c2c6b8d0c5e7136c6
kaveeshabasnayaka <139634637+kaveeshabasnayaka@users.noreply.github.com>
2024-12-02 04:13:39 +0000

    navigation creation

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link,NaviLink } from 'react-router-dom';
+import Home from './components/Home';
+import ContactUs from './components/ContactUs';
+import AboutUs from './components/AboutUs';
+//import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+            <nav>
+              <ul>
+                <li><Link to="/home">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+              </ul>
+            </nav>
+            <Routes>
+              <Route path = '/home' element={<Home/>}/>
+              <Route path = '/about' element={<AboutUs/>}/>
+              <Route path = '/contact' element={<ContactUs/>}/>
+            </Routes>
+          </Router>
     </div>
   );
 }
---
+++ myapp/src/components/ContactUs.js
@@ -0,0 +1,7 @@
+export default function ContactUs(){
+    return(
+        <div>
+            <h1>Welcome to Contact Us Page</h1>
+        </div>
+    );
+}
